94c21d7a90481f67445ff17833b301d268f0e3e0
Nicolas Baglivo <nicolas.baglivo@gmail.com>
2026-01-29 15:17:18 -0300

    (app): build hype

---
+++ app/page.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center font-sans">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center py-32 px-16">
+        <Image
+          src="/logo.png"
+          alt="Medialuna logo"
+          width={100}
+          height={20}
+          priority
+        />
+        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
+          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight">
+            Soon.
+          </h1>
+        </div>
+      </main>
+    </div>
+  );
+}
